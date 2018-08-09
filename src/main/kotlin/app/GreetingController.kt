6c2fc4505acad8450029bf11658f707011600cdf
DJ Duncan <dcduncan.code@gmail.com>
2018-08-09 15:59:18 -0700

    Adds log message

--- src/main/kotlin/app/GreetingController.kt
+++ src/main/kotlin/app/GreetingController.kt
@@ -1,5 +1,7 @@
 package app
 
+import org.slf4j.LoggerFactory
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -8,10 +10,17 @@ import java.util.concurrent.atomic.AtomicLong
 @RestController
 class GreetingController {
 
+	companion object {
+		val LOGGER = LoggerFactory.getLogger(GreetingController::class.java.name)
+	}
+
 	val counter = AtomicLong()
 
+	@CrossOrigin(origins = ["http://localhost:3000"])
 	@GetMapping("/greeting")
-	fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String) =
-			Greeting(counter.incrementAndGet(), "Hello, $name")
+	fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String): Greeting {
+		LOGGER.info("Going to return greeting: Hello, $name")
+		return Greeting(counter.incrementAndGet(), "Hello, $name")
+	}
 
 }
