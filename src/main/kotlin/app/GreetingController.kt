0488b0a489af217bebff3850f90be0f2116131bd
DJ Duncan <dcduncan.code@gmail.com>
2018-08-10 18:10:13 -0700

    Removes logger

--- src/main/kotlin/app/GreetingController.kt
+++ src/main/kotlin/app/GreetingController.kt
@@ -1,6 +1,5 @@
 package app
 
-import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -10,16 +9,11 @@ import java.util.concurrent.atomic.AtomicLong
 @RestController
 class GreetingController {
 
-	companion object {
-		val LOGGER = LoggerFactory.getLogger(GreetingController::class.java.name)
-	}
-
 	val counter = AtomicLong()
 
 	@CrossOrigin(origins = ["http://localhost:3000", "https://itsl1t.herokuapp.com"])
 	@GetMapping("/greeting")
 	fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String): Greeting {
-		LOGGER.info("Going to return greeting: Hello, $name")
 		return Greeting(counter.incrementAndGet(), "Hello, $name")
 	}
 
